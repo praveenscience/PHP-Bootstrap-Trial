f9e11a9bf0cfe9ab5c3d1850f5582fb78be91c31
Praveen Kumar <praveenscience@gmail.com>
2020-03-28 13:08:17 +0000

    Added Bootstrap CSS and updated nav.

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>GSSoC Bootstrap PHP Trial</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
 			<a class="navbar-brand" href="#">Navbar</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -16,16 +17,19 @@
 						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Features</a>
+						<a class="nav-link" href="#">Projects</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Pricing</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						<a class="nav-link" href="#">About Us</a>
 					</li>
 				</ul>
 			</div>
 		</nav>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-6">Left Side</div>
+				<div class="col-6">Right Side</div>
+			</div>
+		</div>
 	</body>
 </html>
