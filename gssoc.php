a7f670a18441df753a497aedeb7e0568d7b1ddbe
Praveen Kumar <praveenscience@gmail.com>
2020-03-28 13:37:25 +0000

    Created pages and sublinks.

--- gssoc.php
+++ gssoc.php
@@ -1,10 +1,10 @@
 <?php include "partials/header.inc"; ?>
-<?php $page = "index.php"; include "partials/nav.inc"; ?>
+<?php $page = "gssoc.php"; include "partials/nav.inc"; ?>
 
 		<div class="container-fluid">
 			<div class="row">
 				<?php include "partials/left.inc"; ?>
-				<div class="col-6">Home Page</div>
+				<div class="col-6">GSSoC Page</div>
 			</div>
 		</div>
 <?php include "partials/footer.inc"; ?>
--- projects.php
+++ projects.php
@@ -1,10 +1,10 @@
 <?php include "partials/header.inc"; ?>
-<?php $page = "index.php"; include "partials/nav.inc"; ?>
+<?php $page = "projects.php"; include "partials/nav.inc"; ?>
 
 		<div class="container-fluid">
 			<div class="row">
 				<?php include "partials/left.inc"; ?>
-				<div class="col-6">Home Page</div>
+				<div class="col-6">Projects</div>
 			</div>
 		</div>
 <?php include "partials/footer.inc"; ?>
