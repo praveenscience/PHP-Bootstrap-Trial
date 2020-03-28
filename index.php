85ae81262f9142054010eea6ad9f4e3631f709b9
Praveen Kumar <praveenscience@gmail.com>
2020-03-28 13:23:52 +0000

    Componentisation using PHP.

--- index.php
+++ index.php
@@ -1,35 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<title>GSSoC Bootstrap PHP Trial</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
-	</head>
-	<body>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Projects</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">About Us</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+<?php include "partials/header.inc"; ?>
+<?php include "partials/nav.inc"; ?>
+
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-6">Left Side</div>
 				<div class="col-6">Right Side</div>
 			</div>
 		</div>
-	</body>
-</html>
+<?php include "partials/footer.inc"; ?>
